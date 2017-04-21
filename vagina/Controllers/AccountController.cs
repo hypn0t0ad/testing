6d872c6f35c4d74716c4386674bfbcef6181f4f3
hypn0t0ad <blackfor3st@gmail.com>
2017-04-21 10:27:43 +0100

    Add project files.

---
+++ vagina/Controllers/AccountController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vagina.Models;
+
+namespace vagina.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            using (OurDBContext db = new OurDBContext())
+            {
+                return View(db.UserAccount.ToList());
+            }
+        }
+
+        public  ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserAccount account)
+        {
+            if (ModelState.IsValid)
+            {
+                using (OurDBContext db = new OurDBContext())
+                {
+                    db.UserAccount.Add(account);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.Message = account.FirstName + " " + account.LastName + " succesfully registered!";
+            }
+            return View();
+        }
+
+        //login 
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserAccount user)
+        {
+            using (OurDBContext db = new OurDBContext())
+            {
+                var usr = db.UserAccount.Single(u => u.UserName == user.UserName && u.Password == user.Password);
+                if (usr != null)
+                {
+                    Session["UserID"] = usr.UserID.ToString();
+                    Session["UserName"] = usr.UserName.ToString();
+                    return RedirectToAction("LogedIn");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "username or password is wrong");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult LogedIn()
+        {
+            if (Session["UserId"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
---
+++ vagina/Models/UserAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace vagina.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage ="Firstname is requiered.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Lastname is requiered.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is requiered.")]
+        //[RegularExpression("")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Username is requiered.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is requiered.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Please comfirm your password.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
