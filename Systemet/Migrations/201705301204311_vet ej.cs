0f1935b03e8670bd9e873c0ef347e119918ec9f9
christian dahlgren <christian.dahlgren84@hotmail.com>
2017-05-30 14:34:15 +0200

    trixat lite här och där!

---
+++ Systemet/Migrations/201705301204311_vet ej.cs
@@ -0,0 +1,18 @@
+namespace Systemet.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class vetej : DbMigration
+    {
+        public override void Up()
+        {
+          
+        }
+        
+        public override void Down()
+        {
+         
+        }
+    }
+}
